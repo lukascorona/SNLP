68538cbd081a546f7c0491b70d80bccd1ece252a
Lukas Laptop <lukasc@mail.upb.de>
2019-11-13 09:07:42 +0100

    output class

---
+++ output/output.py
@@ -0,0 +1,17 @@
+import os
+from datetime import datetime
+
+class Output:
+
+    path = "./output"
+
+    @staticmethod
+    def generateTriple(sentence_id, value):
+        return "<http://swc2017.aksw.org/task2/dataset/{}> <http://swc2017.aksw.org/hasTruthValue> \"{}\"^^<http://www.w3.org/2001/XMLSchema#double> .\n".format(sentence_id, value)
+
+    @staticmethod
+    def generateFile(sentence_ids, values):
+        time = datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")
+        with open("{}/{}.tll".format(Output.path, time), "w") as file:
+            for sentence, value in zip(sentence_ids, values):
+                file.write(Output.generateTriple(sentence, value))
