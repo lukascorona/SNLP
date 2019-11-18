bde973652e61f7974146f4dda6224f26c8c006ee
Dirk Gerhard Leffrang <dirk.leffrang@wiwi.uni-paderborn.de>
2019-11-18 16:40:30 +0100

    textToToken in txtToTriple implemented, test in main added

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from fetch.fetch import Fetcher
 from util.triple import Triple
 from util.predicate import LivesIn
 from util.object import Person, Location
+from txtToTriple.TxtToTriple import TxtToTriple
 
 Fetcher.add(Triple(Person("Lukas"), LivesIn(), Location("Paderborn")))
 Fetcher.add(Triple(Person("Dirk"), LivesIn(), Location("Paderborn")))
 Fetcher.fetch()
+
+tokenizedLine = TxtToTriple.textToToken("Niclas lives in Paderborn")
+for token in tokenizedLine:
+    print(token.text, "-->",token.dep_,"-->", token.pos_)
--- txtToTriple/TxtToTriple.py
+++ txtToTriple/TxtToTriple.py
@@ -1,8 +1,10 @@
+import spacy
 from util.triple import Triple
 
 class TxtToTriple:
     triples = []
     
+    @staticmethod
     def processTxt(txtPath):
         """ Read txt-file """
         fobj = open(txtPath, "r")
@@ -10,18 +12,24 @@ class TxtToTriple:
             processLine(line)
         fobj.close()
 
+    @staticmethod
     def processLine(line):
         """ Process line of txt.file """
-        """ TODO: find triples -> textTiTijeb + tokenToTriples"""
+        """ TODO: find triples -> textToToken + tokenToTriples"""
+        tokenizedLine = textToToken(line)
+        tokenToTriples(tokenizedLine)
         triple = Triple("subject", "predicat", "object")
         triples.append(triple)
 
-	def textToToken():
-		"""TODO: mit spacy/ NLTK """
-		return ""
+    @staticmethod
+    def textToToken(line):
+        """ Requires to install spacy (pip) and en_core_web_sm afterwards (python -m spacy download en_core_web_sm)"""
+        nlp = spacy.load("en_core_web_sm")
+        tokenizedLine = nlp(line)
+        return tokenizedLine
 
-	def tokenToTriples():
-		"""TODO: """
-		return ""
-
+    @staticmethod
+    def tokenToTriples(tokenizedLine):
+        """TODO: mit spacy/ NLTK """
+        return ""
 
